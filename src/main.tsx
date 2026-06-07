12888f2723fc62acd95c648589963bc6b3ef17e6
songye38 <ddev0308@gmail.com>
2026-06-07 19:41:45 +0900

    update : main.tsx에서 BrowserRouter로 감싸주기

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // 1. import 추가
 import './index.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter> {/* 2. 여기서 App을 감싸줍니다 */}
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
